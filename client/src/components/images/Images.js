fefdd0655dc5fb9498821298282ebfeee6872faa
Naomi Prescod-Green <naomipgreen@gmail.com>
2021-03-10 01:23:59 +0000

    update

--- client/src/components/images/Images.js
+++ client/src/components/images/Images.js
@@ -2,27 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-export const Image = ({ src, width, radius, maxWidth, opacity, padding }) => {
-	return (
-		<Img src={src} width={width} maxWidth={maxWidth} radius={radius} opacity={opacity} padding={padding} alt='' />
-	);
-}
+export const Image = ({ src, width, radius, maxWidth, opacity, padding }) => (
+	<Img src={src} width={width} maxWidth={maxWidth} radius={radius} opacity={opacity} padding={padding} alt='' />
+);
 
-export const LinkImage = ({ src, width, maxWidth, link }) => {
-	return (
-		<a href={link}>
-			<Link src={src} width={width} maxWidth={maxWidth} alt='' />
-		</a>
-	);
-}
+export const LinkImage = ({ src, width, maxWidth, link }) => (
+	<a href={link}>
+		<Link src={src} width={width} maxWidth={maxWidth} alt='' />
+	</a>
+);
 
-export const LinkContact = ({ src, link }) => {
-	return (
-		<a href={link}>
-			<ContactLink src={src} link={link} alt='' />
-		</a>
-	);
-}
+export const LinkContact = ({ src, link }) => (
+	<a href={link}>
+		<ContactLink src={src} link={link} alt='' />
+	</a>
+);
 
 // Style component overrides aren't working so I'm passing the styles in via props
 
